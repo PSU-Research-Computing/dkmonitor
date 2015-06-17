86a8fe252fc7951dcfd8baca78225dfe3f6ceec1
Will Patterson <wpatt2@pdx.edu>
2015-06-16 17:01:09 -0700

    Small syntax and bug fixes, Still untested

--- dk_emailer.py
+++ dk_emailer.py
@@ -6,9 +6,9 @@ import settings_obj
 class emailer:
     def __init__(self, user_name, password, suffix):
 
-        self.user_name = user
-        self.password = passw
-        self.suffix = suff
+        self.user_name = user_name
+        self.password = password
+        self.suffix = suffix
 
         self.server = smtplib.SMTP('smtp.gmail.com', 587)
         self.server.ehlo()
@@ -21,10 +21,10 @@ class emailer:
 
 
     def send_email(self, prefix):
-        server.sendmail(self.user_name, prefix + self.suffix, self.message(odin_name))
+        self.server.sendmail(self.user_name, prefix + self.suffix, self.message(prefix))
 
     def message(self, prefix):
-        return_message ="""
+        return_message = """
         Dear {name},
         You have been flagged by ARC's monitoring software for you inaproprate use of the scratch space.
         Please review your usege (delete or move old files, move or delete large files).
