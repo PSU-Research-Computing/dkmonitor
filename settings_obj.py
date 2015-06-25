5995cf67887b58dc82e70308e78bf76f6c3c7ef0
willpatterson <wpatt2@pdx.edu>
2015-06-24 17:10:27 -0700

    Fixed the settings file loading

--- settings_obj.py
+++ settings_obj.py
@@ -1,17 +1,14 @@
 import json
 
-class Settings_interface(self):
+class Settings_interface():
     def __init__(self):
         self.load_file = "settings.json"
-        with json_genrator() as jfile:
-            self.settings = json.load(jfile)
-
-    def json_genrator(self):
         try:
             with open(self.load_file) as jfile:
-                yield jfile
+                self.settings = json.load(jfile)
         except FileNotFoundError as ferror:
             print ("Critical: Json settings file not found")
             raise ferror #probs doesnt work
+
 
 
