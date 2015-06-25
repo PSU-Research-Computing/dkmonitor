e2487ee2f6b35e823f150e8bbd16b17cfa77b3f9
willpatterson <wpatt2@pdx.edu>
2015-06-24 17:12:35 -0700

    Minor changes regarding settings dictionary key changes

--- monitor_manager.py
+++ monitor_manager.py
@@ -11,22 +11,27 @@ class Monitor_manager(settings_obj.Settings_interface):
 
         self.emailer = dk_emailer.emailer(self.settings["Email_API"]["User_name"],
                                           self.settings["Email_API"]["Password"],
-                                          self.settings["Email_API"]["Suffix"])
+                                          self.settings["Email_API"]["User_postfix"])
 
-        self.database = db_interface.data_base(self.settings["DataBase_info"]["dbname"],
+        self.database = db_interface.data_base(self.settings["DataBase_info"]["DataBase"],
                                                self.settings["DataBase_info"]["User_name"],
-                                               self.settings["DataBase_info"]["Password"])
+                                               self.settings["DataBase_info"]["Password"],
+                                               self.settings["DataBase_info"]["Host"])
 
     def run_task(self, task_name):
         task = self.settings["Scheduled_Tasks"][task_name]
         dk_stat_obj = dk_stat.dk_stat(task["System_name"], task["Directory_Path"])
+        print("Searching {path}".format(path=task["Directory_Path"]))
         dk_stat_obj.dir_search()
+        print("Done. Exporting data To database...")
         dk_stat_obj.export_data(self.database)
+        print("Done. Emailing Users")
         dk_stat_obj.email_users(self.emailer,
                                 self.settings["Email_API"]["User_postfix"],
                                 task["Email_flags"]["Access_day_threshold"],
                                 task["Email_flags"]["Total_file_size_threshold"],
                                 task["Email_flags"]["Use_percentage_threshold"])
+        print("Done")
 
 
 
