09fa2b21e3ef31874e0e0fa6514f0569fe1fc5dc
willpatterson <wpatt2@pdx.edu>
2015-07-24 17:42:12 -0700

    Removed useless message function

--- dk_emailer.py
+++ dk_emailer.py
@@ -8,20 +8,8 @@ class emailer:
         self.suffix = suffix
         self.server = smtplib.SMTP('localhost')
 
-
     def send_email(self, message):
         #print("Emailing: " + message["To"])
         self.server.sendmail("Do-Not-Reply", "wpatt2@pdx.edu", message.as_string())
         #self.server.sendmail(self.user_name, message["To"], message.as_string())
-
-    def message(self, prefix):
-        return_message = """
-        Dear {name},
-        You have been flagged by ARC's monitoring software for you inaproprate use of the scratch space.
-        Please review your usege (delete or move old files, move or delete large files).
-        Thank you for your time,
-        --ARC staff
-        """.format(name=prefix)
-
-        return return_message
 
