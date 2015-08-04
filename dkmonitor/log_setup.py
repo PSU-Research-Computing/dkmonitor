b2bbc4663a1d9a00fac2acdf3a0a4b22168cc826
willpatterson <wpatt2@pdx.edu>
2015-08-03 19:05:52 -0700

    Forgot log setup

--- dkmonitor/log_setup.py
+++ dkmonitor/log_setup.py
@@ -5,11 +5,12 @@ File containing simple function that returns log object
 import logging
 import logging.handlers
 
+LOG_ROOT = "~/.dkmonitor/log/"
 
 def setup_logger(log_file_name):
     """Takes log file name as input are returns a logger object"""
 
-    logger = logging.getLogger(log_file_name)
+    logger = logging.getLogger(LOG_ROOT + log_file_name)
     logger.setLevel(logging.INFO)
     handler = logging.handlers.RotatingFileHandler(log_file_name, maxBytes=2048, backupCount=5)
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
