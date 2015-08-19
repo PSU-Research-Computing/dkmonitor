a7649c0098c2c5623a25c9517b4f3e3e91b69e32
willpatterson <wpatt2@pdx.edu>
2015-08-18 17:03:25 -0700

    minor formatting changes

--- dkmonitor/utilities/create_db.py
+++ dkmonitor/utilities/create_db.py
@@ -1,7 +1,6 @@
 """
 Creates Database for dk_monitor
 """
-import psycopg2
 import argparse
 
 import sys, os
@@ -10,7 +9,7 @@ sys.path.append(os.path.abspath("../.."))
 from dkmonitor.utilities.db_interface import DataBase
 
 def create_db(user_name, password, host, db_name):
-    database = DataBase("postgres", user_name, password, host)
+    database = DataBase(db_name, user_name, password, host)
     with database.connect() as cursor:
         cursor.execute("CREATE DATABASE {db} OWNER {user}".format(db=db_name, user=user_name))
         create_tables(cursor)
@@ -21,7 +20,7 @@ def connect_and_create_tables(db_name, user_name, password, host):
         create_tables(cursor)
 
 def create_tables(cursor):
-    table_defs = load_sql_file("../../sql_table_init.sql")
+    table_defs = load_sql_file("sql_table_init.sql")
     cursor.execute(table_defs)
 
 def load_sql_file(sql_file_name):
@@ -44,7 +43,6 @@ def main():
     create_db(args.user, args.passwd, args.host, args.db)
 
 if __name__ == "__main__":
-    #create_db(
     main()
 
 
