3b9066d74e6814fe177f99f8e1f6431d83c1e03b
willpatterson <wpatt2@pdx.edu>
2015-11-10 18:16:13 -0800

    Moved functionallity into taskdatabse object and created export_tasks function

--- dkmonitor/config/task_manager.py
+++ dkmonitor/config/task_manager.py
@@ -6,26 +6,82 @@ sys.path.append(os.path.abspath("../.."))
 from dkmonitor.utilities.new_db_int import Tasks, DataBase
 from dkmonitor.config.settings_manager import export_settings
 
-def load_db():
-    settings = export_settings()
-    return DataBase(hostname=settings["DataBase_Settings"]["host"],
-                    database=settings["DataBase_Settings"]["database"],
-                    password=settings["DataBase_Settings"]["password"],
-                    username=settings["DataBase_Settings"]["user_name"],
-                    db_type=settings["DataBase_Settings"]["database_type"])
+class TaskDataBase(DataBase):
+    def __init__(self, db_settings):
+        super().__init__(hostname=db_settings["host"],
+                         database=db_settings["database"],
+                         password=db_settings["password"],
+                         username=db_settings["user_name"],
+                         db_type=db_settings["database_type"])
+
+    def get_all_tasks(self):
+        session = self.create_session()
+        tasks = session.query(Tasks).all()
+        return tasks
+
+    def get_task_info(self, taskname):
+        session = self.create_session()
+        task = session.query(Tasks).filter(Tasks.taskname==taskname).all()
+        task_info = {}
+        try:
+            task = task[0]
+            for column in task.__table__.columns:
+                task_info[column.name] = getattr(task, column.name)
+            return task_info
+
+        except IndexError as e:
+            return None
+
+    ##INTERFACE METHODS
+    def display_task_info(self, taskname):
+        task_info = self.get_task_info(taskname)
+        display_format = """Task Name: {taskname}
+        Host Name: {hostname}
+        Target Path: {target_path}
+        Relocation Path: {relocation_path}
+        Delete Old Files: {delete_old_files}
+        Delete When Full: {delete_when_full}
+        Disk Usage Warning Threshold: {usage_warning_threshold} %
+        Disk Usage Critical Threshold: {usage_critical_threshold} %
+        Old File Threshold: {old_file_threshold} days
+        Email Usage Warnings: {email_usage_warnings}
+        Email Data Alerations: {email_data_alterations}
+        Email Top Percent: {email_top_percent} %"""
+
+        if task_info is not None:
+            print(display_format.format(**task_info))
+        else:
+            print("Task '{}' does not exist".format(taskname), file=sys.stderr)
+
+    def display_tasks(self):
+        session = self.create_session()
+        tasks = [task for task in session.query(Tasks.taskname).distinct()]
+        if tasks != []:
+            print("Saved tasks:")
+            for task in tasks:
+                print(task[0])
+        else:
+            print("No tasks found", file=sys.stderr)
+
+    def remove_task(self, taskname):
+        session = self.create_session()
+        if session.query(Tasks).filter(Tasks.taskname==taskname).delete() == 1:
+            session.commit()
+            print("Task '{}' was deleted".format(taskname))
+        else:
+            print("Task '{}' does not exist".format(taskname), file=sys.stderr)
 
 def creation_interface():
-    #TODO Throw in do-while loop
     task_input = {"taskname":"",
                   "hostname":"",
                   "target_path":"",
                   "disk_warning_threshold":0,
                   "disk_critical_threshold":0,
-                  "relocate_path":"",
+                  "relocation_path":"",
                   "delete_when_full":False,
-                  "delete_old":False,
+                  "delete_old_files":False,
                   "old_file_threshold":0,
-                  "email_usage_warning":False,
+                  "email_usage_warnings":False,
                   "email_top_percent":0,
                   "email_data_alterations":False}
 
@@ -37,26 +93,29 @@ def creation_interface():
 
     relocate_old = read_bool("Relocate old files when the disk is over it's critical threshold?(y/n): ")
     if relocate_old is True:
-        relocate_path = input("Relocation path: ")
-        task_input["relocate_path"] = relocate_path
+        relocation_path = input("Relocation path: ")
+        task_input["relocation_path"] = relocation_path
         task_input["delete_when_full"] = read_bool("Delete files when relocation path is full?(y/n): ")
     elif relocate_old is False:
-        task_input["delete_old"] = read_bool("Delete old files?(y/n): ")
+        task_input["delete_old_files"] = read_bool("Delete old files?(y/n): ")
 
-    if (relocate_old is True) or (delete_old is True):
+    if (relocate_old is True) or (task_input["delete_old_files"] is True):
         task_input["old_file_threshold"] = read_int("Old file threshold(days)(int): ")
 
     task_input["email_usage_warning"] = read_bool("Send emails when the disk is over it's warning threshold?(y/n): ")
 
-    if email_usage_warnings is True:
+    if task_input["email_usage_warnings"] is True:
         task_input["email_top_percent"] = read_percent("Percent of top users to be emailed(percent)(int): ")
 
-    if (relocate_old is True) or (delete_old is True):
+    if (relocate_old is True) or (task_input["delete_old_files"] is True):
         task_input["email_data_alterations"] = read_bool("Send emails when data has been altered?(y/n): ")
 
     new_task = Tasks(taskname=task_input["taskname"],
                      hostname=task_input["hostname"],
                      target_path=task_input["target_path"],
+                     relocation_path=task_input["relocation_path"],
+                     delete_old_files=task_input["delete_old_files"],
+                     delete_when_full=task_input["delete_when_full"],
                      usage_warning_threshold=task_input["disk_warning_threshold"],
                      usage_critical_threshold=task_input["disk_critical_threshold"],
                      old_file_threshold=task_input["old_file_threshold"],
@@ -71,7 +130,7 @@ def parse_create_command(args):
     new_task = Tasks(taskname=args.taskname,
                      hostname=args.hostname,
                      target_path=args.target_path,
-                     relocate_path=args.relocate_path,
+                     relocation_path=args.relocation_path,
                      delete_old_files=args.delete_old_files,
                      delete_when_full=args.delete_when_full,
                      usage_warning_threshold=args.usage_warning_threshold,
@@ -93,47 +152,105 @@ def get_args():
 
     create_command_parser = subparsers.add_parser("creation_command")
     create_command_parser.set_defaults(which="creation_command")
-    create_command_parser.add_argument("--taskname", dest="taskname", required=True, help="Unique name of the task")
-    create_command_parser.add_argument("--hostname", dest="hostname", required=True, help="Host name of the machine to run the task on")
-    create_command_parser.add_argument("--target_path", dest="target_path", required=True, help="Path of disk/directory to monitor")
+    create_command_parser.add_argument("--taskname",
+                                       dest="taskname",
+                                       required=True,
+                                       help="Unique name of the task")
+    create_command_parser.add_argument("--hostname",
+                                       dest="hostname",
+                                       required=True,
+                                       help="Host name of the machine to run the task on")
+    create_command_parser.add_argument("--target_path",
+                                       dest="target_path",
+                                       required=True,
+                                       help="Path of disk/directory to monitor")
+
     file_management_group = create_command_parser.add_mutually_exclusive_group()
-    file_management_group.add_argument("--relocate_path", dest="relocate_path", help="Path to relocate files to")
-    file_management_group.add_argument("--delete_old_files", dest="delete_old_files", action="store_true", help="Delete old files when disk over critical")
-    create_command_parser.add_argument("--delete_when_full", dest="delete_when_full", action="store_true", help="Delete files when the relocation path is full")
-    create_command_parser.add_argument("--usage_warning_threshold", dest="usage_warning_threshold", type=int, required=True, help="Threshold when warnings should be sent out")
-    create_command_parser.add_argument("--usage_critical_threshold", dest="usage_critical_threshold", type=int, required=True, help="Threshold when files should be moved/deleted")
-    create_command_parser.add_argument("--old_file_threshold", dest="old_file_threshold", type=int, required=True, help="Age of files to be altered when disk is critical")
-    create_command_parser.add_argument("--email_usage_warnings", dest="email_usage_warnings", action="store_true", help="Send emails to users when disk is over threshold")
-    create_command_parser.add_argument("--email_data_alterations", dest="email_data_alterations", action="store_true", help="Send emails when users data has been altered")
-    create_command_parser.add_argument("--email_top_percent", dest="email_top_percent", type=int, defualt=25, help="Percent of users to flag as top users")
+    file_management_group.add_argument("--relocation_path",
+                                       dest="relocation_path",
+                                       help="Path to relocate files to")
+    file_management_group.add_argument("--delete_old_files",
+                                       dest="delete_old_files",
+                                       action="store_true",
+                                       help="Delete old files when disk over critical")
+
+    create_command_parser.add_argument("--delete_when_full",
+                                       dest="delete_when_full",
+                                       action="store_true",
+                                       help="Delete files when the relocation path is full")
+    create_command_parser.add_argument("--usage_warning_threshold",
+                                       dest="usage_warning_threshold",
+                                       type=int,
+                                       required=True,
+                                       help="Threshold when warnings should be sent out")
+    create_command_parser.add_argument("--usage_critical_threshold",
+                                       dest="usage_critical_threshold",
+                                       type=int,
+                                       required=True,
+                                       help="Threshold when files should be moved/deleted")
+    create_command_parser.add_argument("--old_file_threshold",
+                                       dest="old_file_threshold",
+                                       type=int,
+                                       required=True,
+                                       help="Age of files to be altered when disk is critical")
+    create_command_parser.add_argument("--email_usage_warnings",
+                                       dest="email_usage_warnings",
+                                       action="store_true",
+                                       help="Send emails to users when disk is over threshold")
+    create_command_parser.add_argument("--email_data_alterations",
+                                       dest="email_data_alterations",
+                                       action="store_true",
+                                       help="Send emails when users data has been altered")
+    create_command_parser.add_argument("--email_top_percent",
+                                       dest="email_top_percent",
+                                       type=int,
+                                       default=25,
+                                       help="Percent of users to flag as top users")
 
     list_parser = subparsers.add_parser("list")
     list_parser.set_defaults(which="list")
 
     display_parser = subparsers.add_parser("display")
     display_parser.set_defaults(which="display")
-    display_parser.add_argument("taskname", help="Name of task to display")
+    display_parser.add_argument("dtaskname", help="Name of task to display")
 
     remove_parser = subparsers.add_parser("remove")
     remove_parser.set_defaults(which="remove")
-    remove_parser.add_argument("taskname", help="Name of task to remove")
+    remove_parser.add_argument("rtaskname", help="Name of task to remove")
 
     return parser.parse_args()
 
 def main():
     args = get_args()
-    database = load_db()
+    settings = export_settings()
+    taskdb = TaskDataBase(settings["DataBase_Settings"])
 
     if args.which == "creation_interface":
-        database.store_row(creation_interface())
+        taskdb.store_row(creation_interface())
     elif args.which == "creation_command":
-        database.store_row(parse_create_command(args))
+        taskdb.store_row(parse_create_command(args))
     elif args.which == "list":
-        pass
+        taskdb.display_tasks()
     elif args.which == "display":
-        pass
+        taskdb.display_task_info(args.dtaskname)
     elif args.which == "remove":
-        pass
+        taskdb.remove_task(args.rtaskname)
+
+def export_tasks():
+    settings = export_settings()
+    taskdb = TaskDataBase(settings["DataBase_Settings"])
+    raw_tasks = taskdb.get_all_tasks()
+    formatted_tasks = {}
+    try:
+        for task in raw_tasks:
+            task_info = {}
+            for column in task.__table__.columns:
+                task_info[column.name] = getattr(task, column.name)
+            formatted_tasks[task.taskname] = task_info
+
+        return formatted_tasks
+    except IndexError as e:
+        return None
 
 
 ###UTILITY INPUT FUNCTIONS###########
@@ -166,4 +283,7 @@ def read_bool(question):
 
 
 if __name__ == "__main__":
-    main()
+    #main()
+    print(export_tasks())
+
+
