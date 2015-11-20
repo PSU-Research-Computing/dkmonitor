36ced55752cf4fcaa4e32683da78703a87aeca6a
willpatterson <wpatt2@pdx.edu>
2015-11-20 15:39:59 -0800

    Deleted uneeded error and added docstrings

--- dkmonitor/config/settings_manager.py
+++ dkmonitor/config/settings_manager.py
@@ -1,5 +1,4 @@
 import configparser
-
 from configparser import NoOptionError
 from configparser import NoSectionError
 
@@ -8,21 +7,18 @@ sys.path.append(os.path.realpath(__file__)[:os.path.realpath(__file__).rfind("/"
 
 from dkmonitor.utilities.log_setup import setup_logger
 
-class ConfigurationFilesNotFoundError(Exception):
-    def __init__(self, message):
-        super(ConfigurationFilesNotFoundError, self).__init__(message)
-
 class SettingsFileNotFoundError(Exception):
+    """Error for when settings.cfg is not found"""
     def __init__(self, message):
         super(SettingsFileNotFoundError, self).__init__(message)
 
 def load_settings():
+    """Loads settings.cfg into a configparser object"""
     raw_settings = configparser.ConfigParser()
     file_not_found = False
     try:
         raw_settings.read("/etc/dkmonitor.cfg")
     except IOError as e:
-        print("TEST")
         if (e[0] == errno.EPERM):
             print("You do not have permission to read /etc/dkmonitor.cfg", file=sys.stderr)
             file_not_found = True
@@ -34,7 +30,7 @@ def load_settings():
         try:
             raw_settings.read(os.path.join(os.environ["DKM_CONF"], "settings.cfg"))
         except KeyError as e:
-            raise ConfigurationFilesNotFoundError("DKM_CONF is not set, no settings file found")
+            raise SettingsFileNotFoundError("DKM_CONF is not set, no settings file found")
 
     if len(raw_settings) == 1:
         raise SettingsFileNotFoundError("No settings file found at any designated locations")
@@ -42,6 +38,7 @@ def load_settings():
     return raw_settings
 
 def export_settings():
+    """Exports all settings as a dictionary"""
     raw_settings = load_settings()
     formatted_settings = {}
     for section in raw_settings.sections():
@@ -50,10 +47,10 @@ def export_settings():
     return formatted_settings
 
 def section_to_dict(raw_settings, section):
+    """Converts each section of the settings file into a dictionary"""
     formatted_section = dict(raw_settings.items(section))
     for field in formatted_section.keys():
         if formatted_section[field].isdigit():
             formatted_section[field] = int(formatted_section[field])
 
     return formatted_section
-
