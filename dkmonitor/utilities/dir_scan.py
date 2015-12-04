1e9e3de3b61a8a758b2b1b0d607b12be9bcf9ed9
willpatterson <wpatt2@pdx.edu>
2015-12-04 15:14:48 -0800

    Cleaned up dir_scan. replaced path concat with os.join. Got rid of useless error catching

--- dkmonitor/utilities/dir_scan.py
+++ dkmonitor/utilities/dir_scan.py
@@ -1,5 +1,5 @@
 """
-This file is a single function that yeilds every path item in a directory
+This file is a single function that yeilds every file in a directory tree
 """
 
 import os
@@ -7,26 +7,17 @@ import os
 def dir_scan(recursive_dir): #possibly divide into multiple fucntions
     """
     Searches through entire directory tree recursively
-    Saves file info in a dict sorted by user
     """
-    if os.path.isdir(recursive_dir):
-        try:
-            content_list = os.listdir(recursive_dir)
-            for i in content_list:
-                current_path = recursive_dir + '/' + i
-                if os.path.isfile(current_path): #If dir is a file, check when it was modified
-                    yield current_path
-                elif os.path.isdir(current_path):
-                    yield from dir_scan(current_path)
-                else:
-                    pass
-        except PermissionError:
-            pass
-    else:
-        print(recursive_dir)
-        raise OSError
-
-if __name__ == '__main__':
-    for path in dir_scan("/disk/scratch"):
-        print(path)
+    try:
+        content_list = os.listdir(recursive_dir)
+        for path in content_list:
+            current_path = os.path.join(recursive_dir, path)
+            if os.path.isfile(current_path):
+                yield current_path
+            elif os.path.isdir(current_path):
+                yield from dir_scan(current_path)
+            else:
+                pass
+    except PermissionError:
+        pass
 
