78b2e3d23c3fc0590727e9cf3aace09f2f979303
Will Patterson <wpatt2@pdx.edu>
2016-07-08 01:25:32 -0700

    Deleted old direcotry

--- dkmonitor/old/named_tuples.py
+++
@@ -1,6 +0,0 @@
-"""Contains named tuples used in other files"""
-
-from collections import namedtuple
-
-FileTuple = namedtuple('FileTuple', 'file_path file_size last_access')
-StatTuple = namedtuple('StatTuple', 'total_file_size last_access_average')
