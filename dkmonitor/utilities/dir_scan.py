96a45238b7683e95c178ed3b8642e8114f28be53
Will Patterson <wpatt2@pdx.edu>
2016-07-07 20:18:50 -0700

    Moved recursive dir scan func side parent function

--- dkmonitor/utilities/dir_scan.py
+++ dkmonitor/utilities/dir_scan.py
@@ -1,5 +1,5 @@
 """
-This file is a single function that yeilds every file in a directory tree
+Single function that yeilds every file in a directory tree
 """
 
 import os
@@ -9,25 +9,24 @@ def dir_scan(recursive_dir):
     Wrapper function for dir_scan_recursive that throws permission error
     if permisssions are incorrect
     """
+    def dir_scan_generator(recursive_dir):
+        """Searches through entire directory tree recursively"""
+        try:
+            content_list = os.listdir(recursive_dir)
+            for path in content_list:
+                current_path = os.path.join(recursive_dir, path)
+                if os.path.isfile(current_path):
+                    yield current_path
+                elif os.path.isdir(current_path):
+                    yield from dir_scan_generator(current_path)
+                else:
+                    pass
+        except PermissionError:
+            pass
+
     if os.access(recursive_dir, os.R_OK) is True:
-        return dir_scan_recursive(recursive_dir)
+        return dir_scan_generator(recursive_dir)
     else:
         raise PermissionError
 
-def dir_scan_recursive(recursive_dir):
-    """
-    Searches through entire directory tree recursively
-    """
-    try:
-        content_list = os.listdir(recursive_dir)
-        for path in content_list:
-            current_path = os.path.join(recursive_dir, path)
-            if os.path.isfile(current_path):
-                yield current_path
-            elif os.path.isdir(current_path):
-                yield from dir_scan_recursive(current_path)
-            else:
-                pass
-    except PermissionError:
-        pass
 
