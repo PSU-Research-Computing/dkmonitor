e0cfaa4f55864b24bc11a347de8a042db0db847a
Will Patterson <wpatt2@pdx.edu>
2016-07-08 23:14:46 -0700

    Added the start of log setup test

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import unittest
 import os
+import logging
 
 from dkmonitor.utilities.dir_scan import dir_scan
+from dkmonitor.utilities.log_setup import setup_logger
 
 
 DIR_SCAN_DIR = 'test/dir_scan_test'
+LOG_FILE_NAME = 'test/test_log_file.log'
 
 class TestUtilities(unittest.TestCase):
     """Tests for dir_scan fucntions"""
@@ -22,6 +25,11 @@ class TestUtilities(unittest.TestCase):
                               os.path.join(DIR_SCAN_DIR, 'test1.2'),
                               os.path.join(DIR_SCAN_DIR, 'tl2/test2.1'),
                               os.path.join(DIR_SCAN_DIR, 'tl2/test2.2')))
+
+    def test_setup_logger(self):
+        """Test logging setup function"""
+        assert(isinstance(setup_logger(LOG_FILE_NAME), logging.Logger))
+
 
 if __name__ == '__main__':
     test_classes = ()
