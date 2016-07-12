7f7f4464fdc5a4570a37273883130597ba9f4394
Will Patterson <wpatt2@pdx.edu>
2016-07-12 16:38:03 -0700

    Unsure about what has changed

--- dkmonitor/utilities/log_setup.py
+++ dkmonitor/utilities/log_setup.py
@@ -7,26 +7,25 @@ import warnings
 import logging
 from logging import handlers
 
-FIRST_CHOICE = '/var/log/dkmonitor'
-SECOND_CHOICE = os.path.expanduser('~/.dkmonitor/log')
 
-def setup_logger(log_file_name):
+def setup_logger(log_file_path):
     """
     Takes log file name as input are returns a logger object
     TODO:
         Replace print warnings with warnings library
         Move DKM_LOG abstraction outside of setup_logger
     """
-    if os.path.isdir(FIRST_CHOICE) and os.access(FIRST_CHOICE, os.R_OK):
-        log_path = os.path.join(FIRST_CHOICE, log_file_name)
-    elif os.path.isdir(SECOND_CHOICE) and os.access(SECOND_CHOICE, os.R_OK):
-        log_path = os.path.join(SECOND_CHOICE, log_file_name)
+    log_dir = os.path.expanduser(os.path.basename(log_file_path))
+    if os.path.isdir() and os.access(DEFAULT_LOG_PATH, os.R_OK):
+        continue
+    elif os.path.isdir(DEFAULT_LOG_PATH) and os.access(DEFAULT_LOG_PATH, os.R_OK):
+        log_path = DEFAULT_LOG_PATH
     else:
         warnings.warn(("Could Not find log storage directory, Logging to"
                        "current working directory"))
-        log_path = os.path.join(os.path.abspath("."), log_file_name)
+        log_path = os.path.abspath(".")
 
-    log_path = os.path.join(log_path, '{}.log'.format(log_file_name))
+    log_file_path = os.path.join(log_path, log_file_name)
     logger = logging.getLogger(log_path)
     logger.setLevel(logging.INFO)
     handler = handlers.RotatingFileHandler(log_path,
