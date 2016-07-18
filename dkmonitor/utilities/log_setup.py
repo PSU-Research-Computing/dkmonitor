a14f5316839d9f7154448698ec3ef50ef83d91b2
Will Patterson <wpatt2@pdx.edu>
2016-07-18 02:43:09 -0700

    fixed broken log_setup.py

--- dkmonitor/utilities/log_setup.py
+++ dkmonitor/utilities/log_setup.py
@@ -15,23 +15,14 @@ def setup_logger(log_file_path):
         Replace print warnings with warnings library
         Move DKM_LOG abstraction outside of setup_logger
     """
-    log_dir = os.path.expanduser(os.path.basename(log_file_path))
-    if os.path.isdir() and os.access(DEFAULT_LOG_PATH, os.R_OK):
-        continue
-    elif os.path.isdir(DEFAULT_LOG_PATH) and os.access(DEFAULT_LOG_PATH, os.R_OK):
-        log_path = DEFAULT_LOG_PATH
-    else:
-        warnings.warn(("Could Not find log storage directory, Logging to"
-                       "current working directory"))
-        log_path = os.path.abspath(".")
 
-    log_file_path = os.path.join(log_path, log_file_name)
-    logger = logging.getLogger(log_path)
+    logger = logging.getLogger(log_file_path)
     logger.setLevel(logging.INFO)
-    handler = handlers.RotatingFileHandler(log_path,
+    handler = handlers.RotatingFileHandler(log_file_path,
                                            maxBytes=1048576,
                                            backupCount=5)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    formatter = logging.Formatter(("%(asctime)s - %(name)s - "
+                                   "%(levelname)s - %(message)s"))
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
